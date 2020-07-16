d065b04cd8e7777b38d2a45fa740e325388e8e75
yaochenzhi <yaochenzhi@localhost.localdomain>
2020-07-16 23:38:46 +0800

    topic exchange is like direct exchange but has the ability to do routing based on multiple criteria

---
+++ receive_logs_topic.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import pika
+import sys
+from param import parameters
+"""
+Topic exchange
+Topic exchange is powerful and can behave like other exchanges.
+
+When a queue is bound with "#" (hash) binding key - it will receive all the messages, regardless of the routing key - like in fanout exchange.
+
+When special characters "*" (star) and "#" (hash) aren't used in bindings, the topic exchange will behave just like a direct one.
+"""
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+
+channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(
+        exchange='topic_logs', queue=queue_name, routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
