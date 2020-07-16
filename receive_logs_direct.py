d2a8202ab2178a15c6946c0f46e9310d05986e7a
yaochenzhi <yaochenzhi@localhost.localdomain>
2020-07-16 23:17:44 +0800

    multi routing_key message can be binded to the same direct exchange declared but not the default direct exchange

---
+++ receive_logs_direct.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import pika
+import sys
+from param import parameters
+
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+severities = sys.argv[1:]
+if not severities:
+    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
+    sys.exit(1)
+
+# bind 'queue' with 'exchange' with 'routing_key'
+# message with 'routing_key' to the 'exchange'  will be delivered to 'queue'
+
+# the default direct exchange '' can not be binded with 'queue' with specified 'routing_key'
+# but automatically deliver message with routing_key "a" to queue "a" if queue "a" exists.
+for severity in severities:
+    channel.queue_bind(
+        exchange='direct_logs', 
+        queue=queue_name, 
+        routing_key=severity
+    )
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+channel.basic_consume(
+    queue=queue_name, 
+    on_message_callback=callback, 
+    auto_ack=True
+)
+
+channel.start_consuming()
